e81a3d7dc09a9196492c2c7d622326337eb01dff
Rudra Patel <rudrapatel47108@gmail.com>
2026-01-21 15:03:03 +0530

    signup controller added

---
+++ Backend/src/Controllers/auth.controller.js
@@ -0,0 +1,50 @@
+import User from "../Models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+export const signup = async (req, res) => {
+  const { fullname, email, password } = req.body;
+  try {
+    if (!fullname || !email || !password) {
+      return res.status(400).json({ message: "All The fileds are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be atleast 6 characters long" });
+    }
+    //email valid or not so regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email Already Exists" });
+    //hashing of password entered
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      email,
+      password: hashedpassword,
+    });
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+
+      //todo:send a welcome email to the user:- the greeting to get started with the app with a link to the app
+    } else {
+      res.status(400).json({ message: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
---
+++ Backend/src/lib/utils.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+export const generateToken = (userId, res) => {
+  //create a token for the user for authenticity
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, //7 days in milliseconds
+    httpOnly: true, //Prevent XSS Attacks:Cross Site Scripting
+    sameSite: "strict", //CSRF Attacks
+    secure: process.env.NODE_ENV === "development" ? false : true,
+  });
+  return token;
+};
