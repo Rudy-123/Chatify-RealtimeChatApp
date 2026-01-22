8f3b2164486ae753dea7ed4e6f1edee1e9c3f92b
Rudra Patel <rudrapatel47108@gmail.com>
2026-01-22 12:23:37 +0530

    Fix dotenv conflict and update server.js

--- Backend/src/lib/resend.js
+++ Backend/src/lib/resend.js
@@ -1,8 +1,5 @@
 import { Resend } from "resend";
 import { ENV } from "./env.js";
-dotenv.config();
-console.log("NAME:", ENV.EMAIL_FROM_NAME);
-console.log("EMAIL:", ENV.EMAIL_FROM);
 
 export const resendClient = new Resend(ENV.RESEND_API_KEY);
 
