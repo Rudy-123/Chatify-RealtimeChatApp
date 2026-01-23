01c3a79cc2272b301ce04ab0fe45efd0cb6d0b31
Rudra Patel <rudrapatel47108@gmail.com>
2026-01-23 23:53:21 +0530

    rate limiting implemented

--- Backend/src/Routes/auth.route.js
+++ Backend/src/Routes/auth.route.js
@@ -6,8 +6,10 @@ import {
   updateProfile,
 } from "../Controllers/auth.controller.js";
 import { protectRoute } from "../Middleware/auth.middleware.js";
+import { arcjetProtection } from "../Middleware/arcjet.middleware.js";
 
 const router = express.Router();
+router.use(arcjetProtection); //if this passes the req mins per test(rate) and does not get limited then only eveything else can be done
 //POST because we have to create profile on the server and for updating an existing id put request
 router.post("/signup", signup);
 router.post("/login", login);
