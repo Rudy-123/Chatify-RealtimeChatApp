254cfe105aea1c007301334809f71dc69329497c
Rudra Patel <rudrapatel47108@gmail.com>
2026-01-25 23:31:01 +0530

    login and logout implemented

--- Backend/src/Routes/auth.route.js
+++ Backend/src/Routes/auth.route.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.put("/updateProfile", protectRoute, updateProfile); //if user is authenticated then only next function for updating user's profile
+router.put("/updateProfile", protectRoute, arcjetProtection, updateProfile); //if user is authenticated then only next function for updating user's profile
 router.get("/check", protectRoute, (req, res) =>
   res.status(200).json(req.user),
 );
